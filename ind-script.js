488f14257d326627f90df23aeb8680b75fe217be
adityap15 <aditya.ps1515@gmail.com>
2024-08-30 12:39:28 +0530

    added footer section and contact details

--- ind-script.js
+++ ind-script.js
@@ -5,3 +5,31 @@
         hamburger.addEventListener('click', () => {
             navLinks.classList.toggle('active');
         });
+// Back to Top button functionality
+const backToTopButton = document.getElementById("backToTop");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+backToTopButton.onclick = function(e) {
+    e.preventDefault();
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
+
+document.addEventListener('scroll', () => {
+    const section = document.querySelector('.seva-cards');
+    const bounding = section.getBoundingClientRect();
+  
+    if (bounding.top <= 0 && bounding.bottom > window.innerHeight) {
+      section.classList.add('sticky');
+    } else {
+      section.classList.remove('sticky');
+    }
+  });
+  
