bce5d3d7e24c421cb513940362a97beee4335e67
adityap15 <aditya.ps1515@gmail.com>
2024-10-07 14:40:50 +0530

    transitioning in the sevas pages

---
+++ sevas.js
@@ -0,0 +1,19 @@
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const sections = document.querySelectorAll('.section');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    }, {
+        threshold: 0.5
+    });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
